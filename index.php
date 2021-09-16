06ce1963ef24cddcc4001bebdd09c785b460e9aa
rydason <dasen.lai@rakuten-sec.co.jp>
2021-09-16 11:08:52 +0900

    1

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <title>tech boost</title>
+
+    <!-- //ここから追加 -->
+    <style>
+        h1 {
+            color: red;
+        }
+    </style>
+    <!-- ここまで追加 -->
+
+</head>
+
+<body>
+    <h1>Hello world!!!</h1>
+    <p>CSSの入門！</p>
+</body>
+
+</html>
